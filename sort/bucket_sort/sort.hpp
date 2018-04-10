5ab77fb6de8353fdb65e3a7fcce7b6f9ce58f48a
guicunbin <2312570224gui@sina.com>
2018-04-10 11:06:04 +0800

    no change

--- sort/bucket_sort/sort.hpp
+++ sort/bucket_sort/sort.hpp
@@ -40,7 +40,7 @@ vector<int> bucket_sort(vector<int> &nums){
 
     /// ====================== define the BUCKET_NUM and BUCKET_STEP ====================
 
-    int BUCKET_NUM = n ;
+    int BUCKET_NUM = n <= (max_num - min_num + 1) ? n : (max_num - min_num + 1) ;
     int BUCKET_STEP = ceil((max_num - min_num + 1) / BUCKET_NUM);
     cout<<"BUCKET_NUM, BUCKET_STEP = "<<BUCKET_NUM<<","<<BUCKET_STEP<<endl;
 
