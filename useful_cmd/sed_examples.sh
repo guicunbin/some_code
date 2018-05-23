fc558637c297b4d0b71ac14963fa8326e646716b
guicunbin <2312570224gui@sina.com>
2018-05-23 15:46:00 +0800

    add useful_cmd

---
+++ useful_cmd/sed_examples.sh
@@ -0,0 +1,11 @@
+
+sed -i "s|from_detection_checkpoint: true|from_detection_checkpoint: false|g" ${PATH_TO_YOUR_PIPELINE_CONFIG}
+
+sed -i 's|fine_tune_checkpoint_type:  "detection"|from_detection_checkpoint: false|g' ${PATH_TO_YOUR_PIPELINE_CONFIG}
+
+
+
+sed -i "s|"PATH_TO_BE_CONFIGURED/model.ckpt"|"${fine_tune_checkpoint}"|g" ${PATH_TO_YOUR_PIPELINE_CONFIG}
+sed -i "s|PATH_TO_BE_CONFIGURED|"${PATH_TO_BE_CONFIGURED}"|g"  ${PATH_TO_YOUR_PIPELINE_CONFIG}
+sed -i "s| num_steps:| #num_steps:|g" ${PATH_TO_YOUR_PIPELINE_CONFIG}
+sed -i '/eval_config:.*/a\  metrics_set: "coco_detection_metrics"'  ${PATH_TO_YOUR_PIPELINE_CONFIG}
