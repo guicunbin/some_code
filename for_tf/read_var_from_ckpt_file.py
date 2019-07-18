2f6231b905c670aa0c769a9f9bdf7d676f6235e8
guicunbin.19 <guicunbin.19@bytedance.com>
2019-07-18 12:14:46 +0800

    add read_var_from_ckpt_file.py

---
+++ for_tf/read_var_from_ckpt_file.py
@@ -0,0 +1,20 @@
+from tensorflow.python import pywrap_tensorflow
+import os
+import fire
+
+
+def read(checkpoint_path):
+    reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
+    var_to_shape_map = reader.get_variable_to_shape_map()
+    lines = [];
+    for var_name in var_to_shape_map:
+        if ("/weights" in var_name) or  ("/biases" in var_name):
+            if ("/weights/" in var_name) or  ("/biases/" in var_name): continue;
+            lines.append(">> {}: {}".format(var_name, reader.get_tensor(var_name).shape))
+    lines.sort();
+
+    for line in lines:
+        print(line);
+
+
+fire.Fire(read);
